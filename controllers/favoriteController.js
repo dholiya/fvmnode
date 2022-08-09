733a339a20742a238d82f9bf91c059baf0a77e74
dholiya <krunaldholiya1750@gmail.com>
2022-08-08 22:38:23 -0400

    fav: add and remove

--- controllers/favoriteController.js
+++ controllers/favoriteController.js
@@ -5,14 +5,40 @@ const Util = require('../Util/Util');
 const favorite_add = (req, res) => {
     console.log(req.files);
     const favorite = new Favorite(req.body);
-    favorite.save()
-        .then(result => {
-            res.json({ status: 200,msg:"Product added in favorite successfully" });
-        })
+
+
+    Favorite.findOne({ user_id: req.body.user_id,product_id: req.body.product_id }).then(result => {
+        if (result != null && result.length != 0) {
+           
+            Favorite.findOneAndDelete({user_id: req.body.user_id, product_id: req.body.product_id })
+            .then(result => {
+                if (result != null && result.length != 0) 
+                    res.json({ status: 200,  msg: 'Product remove from favorite successfully'});
+                else
+                res.json({ status: 300, msg: "No favorite found"});
+            })
+            .catch(err => {
+                res.json({ status: 400,  msg: err.message});
+            });
+
+        } else {
+            favorite.save()
+            .then(result => {
+                res.json({ status: 200, msg:"Product added in favorite successfully" });
+            })
+            .catch(err => {
+                res.json({ status: 400, msg: err.message })
+                console.log(err);
+            });
+        }
+      })
         .catch(err => {
-            res.json({ status: 400, msg: err.message })
-            console.log(err);
+          res.json({ status: 400, msg: err.message });
         });
+
+
+
+   
 }
 
 
